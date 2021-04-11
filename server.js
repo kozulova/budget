63501389b1e6b80c415fb7658502048e7752cdcb
kozulova <benedukvalia@gmail.com>
2021-04-11 15:00:12 +0300

    production build

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ connectDB();
 app.use('/expenses', router);
 
 if(process.env.NODE_ENV === 'production'){
-
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html')))
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>console.log(`Server is lisening on port ${PORT}`))
