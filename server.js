8935655c188726ce72a52fcb73951a361ac94c35
kozulova <benedukvalia@gmail.com>
2021-04-11 15:39:31 +0300

    build changes

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const router = require('./routes/expenses');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -16,9 +18,9 @@ app.use('/expenses', router);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-    //app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>console.log(`Server is lisening on port ${PORT}`))
