53c87819307c49bb808af6c593aeff0697c139e1
kozulova <benedukvalia@gmail.com>
2021-04-12 14:06:23 +0300

    style changes

--- client/src/components/Expense.js
+++ client/src/components/Expense.js
@@ -3,12 +3,11 @@ import { GlobalContext } from '../context/GlobalState'
 
 const Expense = ({expense}) => {
     const {deleteExpence} = useContext(GlobalContext);
-    console.log(expense._id);
     return (
             <li className="expense" key={expense._id}>
-            <div>{expense.category}  {expense.amount}</div>
+            <div><strong>{expense.category}</strong> - ${expense.amount}</div>
             <div>
-            <button>EDIT</button> <button onClick={()=>deleteExpence(expense._id)}>DELETE</button>
+            <button onClick={()=>deleteExpence(expense._id)}>DELETE</button>
             </div> 
             </li>
     )
